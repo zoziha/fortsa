cce4646e7a09b8a11046de6d6012a9c0ab91d8b2
zoziha13 <1325686572@qq.com>
2021-06-21 16:06:26 +0800

    daily

--- test/test.f90
+++ test/test.f90
@@ -54,71 +54,19 @@ program main
 
     endblock
 
-    ! block   !! arimatest.c
-    !     use forlab, only: disp, file
-    !     use fortran_tsa, only: arima_set, arima_init, &
-    !                             arima_setMethod,arima_exec, &
-    !                             arima_summary, arima_predict, &
-    !                             arima_free, arima_setOptMethod
-    !     use, intrinsic :: iso_c_binding
-    !     integer :: i, d, L
-    !     integer :: p, q
-    !     real(8), target, allocatable :: xpred(:), amse(:)
-    !     type(c_ptr) :: obj
-    !     type(arima_set), target :: set
-    !     ! target set
-    !     ! type(arima_set) :: obj
-    !     type(file) :: infile
-    !     integer :: line
-    !     real(8), target, allocatable :: inp(:)
-
-    !     p = 0
-    !     d = 1
-    !     q = 0
-
-    !     L = 0
-
-    !     line = 0
-
-    !     infile = file('data/seriesA.txt')
-    !     call infile%open('r')
-    !     line = infile%countlines()
-    !     allocate(inp(line), xpred(L), amse(L))
-
-    !     do i = 1, line
-    !         read(infile%unit, *) inp(i)
-    !     enddo
-
-    !     ! obj = c_loc(set)
-    !     ! call c_f_pointer(obj, set)
-
-    !     obj = arima_init(p, d, q, line)
-    !     call arima_setMethod(obj, 0)
-    !     call arima_setOptMethod(obj, 5)
-    !     call arima_exec(obj, c_loc(inp(1)))
-    !     call arima_summary(obj)
-    !     call arima_predict(obj, c_loc(inp(1)), L, c_loc(xpred(1)), c_loc(amse(1)))
-        
-    !     call disp('Predicted Values : ')
-    !     call disp(xpred)
-    !     call disp('Standard Errors : ')
-    !     call disp(sqrt(amse))
-
-    !     ! call arima_free(obj)
-    !     deallocate(inp, xpred, amse)
-    !     call infile%close()
-
-    ! endblock
-
     block   !! arimatest.c
         use forlab, only: disp, file
-        use fortran_tsa_test, only: arima_set, arima_init
+        use fortran_tsa, only: arima_set, arima_init, &
+                                arima_setMethod,arima_exec, &
+                                arima_summary, arima_predict, &
+                                arima_free, arima_setOptMethod
         use, intrinsic :: iso_c_binding
         integer :: i, d, L
         integer :: p, q
         real(8), target, allocatable :: xpred(:), amse(:)
-        type(arima_set) :: obj
-        type(arima_set), pointer :: set
+        type(c_ptr) :: obj = c_null_ptr
+        target obj
+        ! type(arima_set), target :: set
         ! target set
         ! type(arima_set) :: obj
         type(file) :: infile
@@ -146,7 +94,19 @@ program main
         ! call c_f_pointer(obj, set)
 
         obj = arima_init(p, d, q, line)
-        print *, obj%N
+        call arima_setMethod(obj, 0)
+        call arima_setOptMethod(obj, 5)
+        call arima_exec(obj, c_loc(inp(1)))
+        call arima_summary(obj)
+        call arima_predict(obj, c_loc(inp(1)), L, c_loc(xpred(1)), c_loc(amse(1)))
+        
+        call disp('Predicted Values : ')
+        call disp(xpred)
+        call disp('Standard Errors : ')
+        call disp(sqrt(amse))
+
+        ! call arima_free(obj)
+        deallocate(inp, xpred, amse)
         call infile%close()
 
     endblock
