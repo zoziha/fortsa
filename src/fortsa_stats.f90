f9d956d04de4df2ddc09af5f0eb055e4b230c3f2
zoziha <zuo.zhihua@qq.com>
2021-12-10 22:40:51 +0800

    Update fortsa_stats

--- src/fortsa_stats.f90
+++ src/fortsa_stats.f90
@@ -1,6 +1,7 @@
 module fortsa_stats
 
     use, intrinsic :: iso_c_binding, only: c_int, c_double
+    use, intrinsic :: iso_fortran_env, only: dp => real64, int32
     implicit none
     private
 
@@ -8,47 +9,91 @@ module fortsa_stats
     public :: pacf, pacf_opt
 
     interface
-        !> Auto Covariance Function
-        subroutine acvf(vec, N, par, M) bind(c, name='acvf')
+        subroutine ctsa_acvf(vec, N, par, M) bind(c, name='acvf')
             import c_int, c_double
             real(kind=c_double), intent(in) :: vec(*)
             real(kind=c_double), intent(out) :: par(*)
             integer(kind=c_int), value, intent(in) :: N, M
-        end subroutine acvf
+        end subroutine ctsa_acvf
 
-        !> Method 0 : Regular Method. Slow for large input length.
-        !> Method 1 : FFT based Method. Use it if data length is large
-        subroutine acvf_opt(vec, N, method, par, M) bind(c, name='acvf_opt')
+        subroutine ctsa_acvf_opt(vec, N, method, par, M) bind(c, name='acvf_opt')
             import c_int, c_double
             real(kind=c_double), intent(in) :: vec(*)
             real(kind=c_double), intent(out) :: par(*)
             integer(kind=c_int), value, intent(in) :: N, method, M
-        end subroutine acvf_opt
+        end subroutine ctsa_acvf_opt
 
-        !> Converts Autocovariance to autocorrelation function
-        pure subroutine acvf2acf(acf, M) bind(c, name='acvf2acf')
+        pure subroutine ctsa_acvf2acf(acf, M) bind(c, name='acvf2acf')
             import c_int, c_double
             real(kind=c_double), intent(inout) :: acf(*)
             integer(kind=c_int), value, intent(in) :: M
-        end subroutine acvf2acf
+        end subroutine ctsa_acvf2acf
 
-        !> Partial Auto Covariance Function
-        pure subroutine pacf(vec, N, par, M) bind(c, name='pacf')
+        pure subroutine ctsa_pacf(vec, N, par, M) bind(c, name='pacf')
             import c_int, c_double
             real(kind=c_double), intent(in) :: vec(*)
             real(kind=c_double), intent(out) :: par(*)
             integer(kind=c_int), value, intent(in) :: N, M
-        end subroutine pacf
-
-        !> Method 0 : Yule-Walker
-        !> Method 1 : Burg
-        !> Method 2 : Box-Jenkins Conditional MLE
-        subroutine pacf_opt(vec, N, method, par, M) bind(c, name='pacf_opt')
+        end subroutine ctsa_pacf
+        
+        subroutine ctsa_pacf_opt(vec, N, method, par, M) bind(c, name='pacf_opt')
             import c_int, c_double
             real(kind=c_double), intent(in) :: vec(*)
             real(kind=c_double), intent(out) :: par(*)
             integer(kind=c_int), value, intent(in) :: N, method, M
-        end subroutine pacf_opt
+        end subroutine ctsa_pacf_opt
     end interface
+    
+contains
+
+    !> Auto Covariance Function
+    subroutine acvf(vec, par)
+        real(kind=dp), intent(in) :: vec(:)
+        real(kind=dp), intent(out) :: par(:)
+        
+        call ctsa_acvf(vec, size(vec), par, size(par))
+        
+    end subroutine acvf
+    
+    !> Method 0 : Regular Method. Slow for large input length.
+    !> Method 1 : FFT based Method. Use it if data length is large
+    subroutine acvf_opt(vec, method, par)
+        real(kind=dp), intent(in) :: vec(:)
+        integer(int32), intent(in) :: method
+        real(kind=dp), intent(out) :: par(:)
+        
+        call ctsa_acvf_opt(vec, size(vec), method, par, size(par))
+        
+    end subroutine acvf_opt
+    
+    !> Converts Autocovariance to autocorrelation function
+    subroutine acvf2acf(vec)
+        real(kind=dp), intent(inout) :: vec(:)
+        
+        call ctsa_acvf2acf(vec, size(vec))
+        
+    end subroutine acvf2acf
+    
+    !> Partial Auto Covariance Function
+    subroutine pacf(vec, par)
+        real(kind=dp), intent(in) :: vec(:)
+        real(kind=dp), intent(out) :: par(:)
+        
+        call ctsa_pacf(vec, size(vec), par, size(par))
+        
+    end subroutine pacf
+    
+    !> Method 0 : Yule-Walker
+    !> Method 1 : Burg
+    !> Method 2 : Box-Jenkins Conditional MLE
+    subroutine pacf_opt(vec, method, par)
+        real(kind=dp), intent(in) :: vec(:)
+        integer(int32), intent(in) :: method
+        real(kind=dp), intent(out) :: par(:)
+        
+        call ctsa_pacf_opt(vec, size(vec), method, par, size(par))
+        
+    end subroutine pacf_opt
+    
 
 end module fortsa_stats
--- tests/stats/test_stats.f90
+++ tests/stats/test_stats.f90
@@ -42,27 +42,27 @@ contains
         call infile%close()
 
         acf_ = [4.223, 4.128, 3.987, 3.810, 3.608, 3.388, 3.157, 2.922, 2.683, 2.444]
-        call acvf(inp, infile%lines, acf, 10)
+        call acvf(inp, acf)
         do i = 1, 10
             call check(error, acf(i), acf_(i), thr=0.001_8)
             if (allocated(error)) return
         end do
 
-        call acvf_opt(inp, infile%lines, 0, acf, 10)
+        call acvf_opt(inp, 0, acf)
         do i = 1, 10
             call check(error, acf(i), acf_(i), thr=0.001_8)
             if (allocated(error)) return
         end do
 
         !> FFT Based Method
-        call acvf_opt(inp, infile%lines, 1, acf, 10)
+        call acvf_opt(inp, 1, acf)
         do i = 1, 10
             call check(error, acf(i), acf_(i), thr=0.001_8)
             if (allocated(error)) return
         end do
 
         acf_ = [1.000, 0.9776, 0.9441, 0.9022, 0.8543, 0.8024, 0.7476, 0.6919, 0.6354, 0.5788]
-        call acvf2acf(acf, 10)
+        call acvf2acf(acf)
         do i = 1, 10
             call check(error, acf(i), acf_(i), thr=0.001_8)
             if (allocated(error)) return
@@ -94,14 +94,14 @@ contains
         pacf_ = [0.9776, -0.2602, -0.1570, -0.9331E-01, -0.5745E-01, &
                  -0.4563E-01, -0.1217E-01, -0.3751E-01, -0.2236E-01, -0.9845E-02]
         !> Default Method is Yule-Walker
-        call pacf(inp, size(inp), par, size(par))
+        call pacf(inp, par)
         do i = 1, 10
             call check(error, par(i), pacf_(i), thr=0.001_8)
             if (allocated(error)) return
         end do
 
         !> pacf_opt : Method 0 Yule Walker
-        call pacf_opt(inp, size(inp), 0, par, size(par))
+        call pacf_opt(inp, 0, par)
         do i = 1, 10
             call check(error, par(i), pacf_(i), thr=0.001_8)
             if (allocated(error)) return
@@ -110,7 +110,7 @@ contains
         pacf_ = [0.9999, -0.8173, 0.1115E-01, 0.1499E-01, -0.5958E-01, &
                  -0.3016E-01, 0.3299E-01, 0.6476E-02, 0.8042E-01, -0.7123E-02]
         !> pacf_opt : Method 1 Burg
-        call pacf_opt(inp, size(inp), 1, par, size(par))
+        call pacf_opt(inp, 1, par)
         do i = 1, 10
             call check(error, par(i), pacf_(i), thr=0.001_8)
             if (allocated(error)) return
@@ -119,7 +119,7 @@ contains
         !> pacf_opt : Method 2 MLE (Box-Jenkins)
         pacf_ = [1.000, -0.8325, -0.2928E-01, -0.2786E-01, -0.1042, &
                  -0.7958E-01, -0.8415E-02, -0.3885E-01, 0.4349E-01, -0.4086E-01]
-        call pacf_opt(inp, size(inp), 2, par, size(par))
+        call pacf_opt(inp, 2, par)
         do i = 1, 10
             call check(error, par(i), pacf_(i), thr=0.001_8)
             if (allocated(error)) return
